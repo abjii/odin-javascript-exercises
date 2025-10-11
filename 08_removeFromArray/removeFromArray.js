20e1c7dfe63f938d7c4e2d8ff95725e1102a788d
abjii <156369372+abjii@users.noreply.github.com>
2025-10-11 18:58:02 +0200

    array functions js

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+	args.forEach(function(arg) {
+	let index =0;
+	do{
+	index = array.findIndex(item => item === arg);
+	if (index>=0){array.splice(index,1);}
+	}while (index>=0)	
+	})
+	
+	return (array);
 };
 
 // Do not edit below this line
