0ceaa226b1c8b3e879c853170bd37b194e133c65
abjii <156369372+abjii@users.noreply.github.com>
2025-10-15 21:32:14 +0200

    tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+	let result = (temp-32)*5/9;
+	return (Math.round(result*10)/10);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+	let result = temp*9/5+32
+	return (Math.round(result*10)/10);
 };
+
+function formatToOneDecimalNoRound(number) {
+  const multiplied = number * 10;
+  const truncated = Math.trunc(multiplied);
+  const result = truncated / 10;
+  return result;
+}
 
 // Do not edit below this line
 module.exports = {
