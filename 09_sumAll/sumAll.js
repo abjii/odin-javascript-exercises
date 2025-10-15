64187d838cccc28d5021ac25fdcead92a9b5b355
abjii <156369372+abjii@users.noreply.github.com>
2025-10-15 20:25:34 +0200

    sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+if ((a<0) || (b<0) || (!Number.isInteger(a)) || (!Number.isInteger(b))) return 'ERROR'
+let sum = 0;
+if (a<b){
+	let i = a;
+	while(i<=b){
+	sum += i;
+	i++;}
+}
+else if (a>b){
+	let i = a;
+	while(i>=b){
+	sum += i;
+	i--;}
+}
+return sum;
 };
 
 // Do not edit below this line
