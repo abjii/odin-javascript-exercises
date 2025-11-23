d1b9098da299846fc1c7454c200acbf62d8c8776
abjii <156369372+abjii@users.noreply.github.com>
2025-11-23 11:54:03 +0100

    add calculator functions

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -7,14 +7,33 @@ const subtract = function (a, b) {
 };
 
 const sum = function (arr) {
-return (arr.reduce((sum, item) => {sum+=item;return sum;},0));
+  return arr.reduce((sum, item) => {
+    sum += item;
+    return sum;
+  }, 0);
 };
 
-const multiply = function () {};
+const multiply = function (arr) {
+  return arr.reduce((res, item) => {
+    res *= item;
+    return res;
+  }, 1);
+};
 
-const power = function () {};
+const power = function (a, b) {
+  return Math.pow(a, b);
+};
 
-const factorial = function () {};
+const factorial = function (a) {
+  if (a === 0) return 1;
+  else {
+    let res = 1;
+    for (let i = 1; i <= a; i++) {
+      res *= i;
+    }
+    return res;
+  }
+};
 
 // Do not edit below this line
 module.exports = {
