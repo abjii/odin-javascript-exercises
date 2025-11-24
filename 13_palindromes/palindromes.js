169f7a3763c619a349d336b6eb798d20424fb667
abjii <156369372+abjii@users.noreply.github.com>
2025-11-24 10:10:50 +0100

    add palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+  let cleanPhrase = phrase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g, "");
+  for (let i = 0; i < cleanPhrase.length / 2; i++) {
+    if (cleanPhrase[i].toUpperCase().localeCompare(cleanPhrase[cleanPhrase.length - i - 1].toUpperCase()) != 0) return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
