5c6ee99a7efd235f2d8e7e66c5732d0297516864
abjii <156369372+abjii@users.noreply.github.com>
2025-11-25 16:42:06 +0100

    add fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function fib(a) {
+  if (a < 0) return "OOPS";
+  if (a == 0) return 0;
+  else if (a == 1) return 1;
+  return fib(a - 1) + fib(a - 2);
 };
 
 // Do not edit below this line
