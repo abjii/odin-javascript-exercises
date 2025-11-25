6a5a29ad451a2d269ff0fd9b338bcde0741a72e9
abjii <156369372+abjii@users.noreply.github.com>
2025-11-25 20:47:52 +0100

    add function findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const currentYear = new Date().getFullYear();
+const findTheOldest = function (people) {
+  return people.sort((a, b) => {
+    if (a.yearOfDeath === undefined) a.yearOfDeath = currentYear;
+    if (b.yearOfDeath === undefined) b.yearOfDeath = currentYear;
+    bage = b.yearOfDeath - b.yearOfBirth;
+    aage = a.yearOfDeath - a.yearOfBirth;
+    return bage - aage;
+  })[0];
 };
 
 // Do not edit below this line
